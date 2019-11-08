1580c74d560c90a174972b64616a1c7b44605348
peng <>
2019-11-08 21:21:18 +0800

    1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,7 +22,7 @@ export default class Home extends Component<Props, State> {
   }
 
   sleep = async () => {
-    return await new Promise(resolve => {
+    await new Promise(resolve => {
       setTimeout(() => {
         resolve(666);
       }, 1000);
